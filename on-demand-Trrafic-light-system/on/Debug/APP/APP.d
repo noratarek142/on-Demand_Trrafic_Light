fd195d2a5fe1502c2bc57c0dce8168b3d45969bc
noratarek142 <105989161+noratarek142@users.noreply.github.com>
2022-10-23 02:42:38 +0200

    Add files via upload

---
+++ on-demand-Trrafic-light-system/on/Debug/APP/APP.d
@@ -0,0 +1,30 @@
+APP/APP.o APP/APP.o: ../APP/APP.c ../APP/APP.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/DIO/DIO.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/UTILITIES/STD.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/UTILITIES/BitMath.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/TIMER/TIMER.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on\MCAL/DIO/DIO.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/INTERRUPT/INTERRUPT.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on\UTILITIES/BitMath.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/ECUAL/Buttons/BUTTONS.h \
+  C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/ECUAL/LED/LED.h
+
+../APP/APP.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/DIO/DIO.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/UTILITIES/STD.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/UTILITIES/BitMath.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/TIMER/TIMER.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on\MCAL/DIO/DIO.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/MCAL/INTERRUPT/INTERRUPT.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on\UTILITIES/BitMath.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/ECUAL/Buttons/BUTTONS.h:
+
+C:\Users\norat\OneDrive\Documents\NTI_GROUP\workshop\on/ECUAL/LED/LED.h:
---
+++ on-demand-Trrafic-light-system/on/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 20, 2022
+ *      Author: norat
+ */
+
+#include"BUTTONS.h"
+#include"LED.h"
+#include <util/delay.h>
+#include"APP/APP.h"
+//#include "TestMain_LED.h"
+//#include"TestMain_BUTTONS.h"
+//#include"TestMain_TImer.h"
+int main (void)
+{
+	//testing_timers();
+	//testing_buttons();
+	//testing_leds();
+	APP_Void_AppInit();
+		 while (1)
+		 {
+
+			 APP_Void_AppStart();
+		 }
+
+		 return 0;
+}
+
+
